c57a544f8d72918dc2312b4ed58d4eeb5b0c5cf5
janghyoseog <bestdevhyo1225@gmail.com>
2020-03-24 22:45:36 +0900

    [#4] feat: step-2 grpc 예제 추가

---
+++ step-2-insert-books/server.js
@@ -0,0 +1,25 @@
+const protoLoader = require('@grpc/proto-loader');
+const grpc = require('grpc');
+
+const packageDefinition = protoLoader.loadSync('books.proto');
+const booksProto = grpc.loadPackageDefinition(packageDefinition).books;
+
+const server = new grpc.Server();
+
+const books = [
+    { id: 123, title: 'A Tale of Two Cities', author: 'Charles Dickens' }
+];
+
+server.addService(booksProto.BookService.service, {
+    list: (call, callback) => { callback(null, { books }); },
+    insert: (call, callback) => {
+        books.push(call.request);
+        callback(null, {});
+    }
+});
+
+server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+
+console.log('Server running at http://0.0.0.0:50051');
+
+server.start();
